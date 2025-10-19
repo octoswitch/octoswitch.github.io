464298a55e5d88cb5686657897e9fa665e0dde79
Max Davy <max@davy.engineer>
2025-10-19 13:48:35 -0500

    Add vanity image and 3d view

--- styles/material.js
+++ styles/material.js
@@ -3063,32 +3063,34 @@
         var top = props.top + props.height / 2;
         var marginLeft = -1 * (this.element_.offsetWidth / 2);
         var marginTop = -1 * (this.element_.offsetHeight / 2);
-        if (this.element_.classList.contains(this.CssClasses_.LEFT) || this.element_.classList.contains(this.CssClasses_.RIGHT)) {
-            left = props.width / 2;
-            if (top + marginTop < 0) {
-                this.element_.style.top = '0';
-                this.element_.style.marginTop = '0';
+        if (!this.element_.classList.contains("static-tooltip")) {/* added by MD 10-19-25*/
+            if (this.element_.classList.contains(this.CssClasses_.LEFT) || this.element_.classList.contains(this.CssClasses_.RIGHT)) {
+                left = props.width / 2;
+                if (top + marginTop < 0) {
+                    this.element_.style.top = '0';
+                    this.element_.style.marginTop = '0';
+                } else {
+                    this.element_.style.top = top + 'px';
+                    this.element_.style.marginTop = marginTop + 'px';
+                }
             } else {
-                this.element_.style.top = top + 'px';
-                this.element_.style.marginTop = marginTop + 'px';
+                if (left + marginLeft < 0) {
+                    this.element_.style.left = '0';
+                    this.element_.style.marginLeft = '0';
+                } else {
+                    this.element_.style.left = left + 'px';
+                    this.element_.style.marginLeft = marginLeft + 'px';
+                }
             }
-        } else {
-            if (left + marginLeft < 0) {
-                this.element_.style.left = '0';
-                this.element_.style.marginLeft = '0';
+            if (this.element_.classList.contains(this.CssClasses_.TOP)) {
+                this.element_.style.top = props.top - this.element_.offsetHeight - 10 + 'px';
+            } else if (this.element_.classList.contains(this.CssClasses_.RIGHT)) {
+                this.element_.style.left = props.left + props.width + 10 + 'px';
+            } else if (this.element_.classList.contains(this.CssClasses_.LEFT)) {
+                this.element_.style.left = props.left - this.element_.offsetWidth - 10 + 'px';
             } else {
-                this.element_.style.left = left + 'px';
-                this.element_.style.marginLeft = marginLeft + 'px';
+                this.element_.style.top = props.top + props.height + 10 + 'px';
             }
-        }
-        if (this.element_.classList.contains(this.CssClasses_.TOP)) {
-            this.element_.style.top = props.top - this.element_.offsetHeight - 10 + 'px';
-        } else if (this.element_.classList.contains(this.CssClasses_.RIGHT)) {
-            this.element_.style.left = props.left + props.width + 10 + 'px';
-        } else if (this.element_.classList.contains(this.CssClasses_.LEFT)) {
-            this.element_.style.left = props.left - this.element_.offsetWidth - 10 + 'px';
-        } else {
-            this.element_.style.top = props.top + props.height + 10 + 'px';
         }
         this.element_.classList.add(this.CssClasses_.IS_ACTIVE);
     };
